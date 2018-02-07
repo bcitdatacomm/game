f144ee9147ea9c28e256a1a98283f5415e9b96ee
julianafrench <julianaefrench@gmail.com>
2018-02-07 13:11:08 -0800

    Added minimal other player object for game server to update

---
+++ Assets/Scripts/OtherPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For rendering other players when needed.
+public class OtherPlayer : MonoBehaviour {
+
+    public string displayName;
+    public Transform playerPos;
+    public float x_coordinate;
+    public float y_coordinate;
+
+    Rigidbody playerRigidbody;
+    // Use this for initialization
+    void Start () {
+        x_coordinate = playerPos.transform.position.x;
+        y_coordinate = playerPos.transform.position.y;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        x_coordinate = playerPos.transform.position.x;
+        y_coordinate = playerPos.transform.position.y;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+}
