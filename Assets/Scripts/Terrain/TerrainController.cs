b559f559e210c2b39433fece3bed6b832137eacf
Krixium <bennywang75@hotmail.com>
2018-02-16 21:45:02 -0800

    Added skeleton terrain controller

---
+++ Assets/Scripts/Terrain/TerrainController.cs
@@ -0,0 +1,49 @@
+﻿public class TerrainController {
+
+    enum TileTypes {
+        GROUND,
+        CACTUS,
+        BUSH
+    };
+
+    public struct Encoding
+    {
+        int[,] tiles;
+        Building[] buildings;
+    };
+
+    public Encoding Data { get; set; }
+
+    public long Width { get; set; }
+    public long Height { get; set; }
+    public long TileSize { get; set; }
+
+    public double CactusCoeff { get; set; }
+    public double BushCoeff { get; set; }
+
+    public Building[] Buildings { get; set; }
+
+    TerrainController()
+    {
+
+    }
+
+    public bool GenerateEncoding()
+    {
+        return false;
+    }
+
+    public bool Instantiate()
+    {
+        return false;
+    }
+}
+
+
+public class Building
+{
+    Building()
+    {
+
+    }
+}
