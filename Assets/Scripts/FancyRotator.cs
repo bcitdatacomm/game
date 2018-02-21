56794848b39cad6a2a24647f55b8c5544bcd608a
OPWonShinobi <jxia11@my.bcit.ca>
2018-02-21 01:52:04 -0800

    added basic pickup template to Vafa's attackable scene

---
+++ Assets/Scripts/FancyRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancyRotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime );    		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
