79092861bfefe08cd49e7f68ab64bd417970e4dc
Cooluke75 <luke.lee7575@gmail.com>
2018-02-23 10:37:08 -0800

    Bullet script added
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        //var health = hit.GetComponent<Health>();
+        var health = hit.health;
+        if (health != null)
+        {
+            health -= 10;
+        }
+
+        Destroy(gameObject);
+    }
+}
