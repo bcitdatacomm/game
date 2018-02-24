e9963bf14694a74d84fbbe480327054607101a2c
wozenwho <wilson.huu@gmail.com>
2018-02-24 14:00:04 -0800

    Slightly modified connection.cs - write and writetobuffer methods

--- Networking/src/Connection.cs
+++ Networking/src/Connection.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Net.Sockets;
 
-namespace Connection
+namespace COMP4981_NetworkingTest
 {
-	
-	[StructLayout(LayoutKind.Explicit, Pack = 1)]
-	public struct CAddr{
+
+    	[StructLayout(LayoutKind.Explicit, Pack = 1)]
+	    public struct CAddr{
 
 		public static readonly CAddr Loopback = new CAddr("127.0.0.1");
 
@@ -48,48 +48,66 @@ namespace Connection
 
 	}
 
- 
-
-/*    public unsafe struct buffer_t{
-        public char[] buffer = new char[2048];
-        public int bufPos;
+    enum connectionStatus : byte
+    {
+        disconnected = 0,
+        connection_bad = 1,
+        connection_good = 2,
     }
-*/
+
+    public unsafe struct sockaddr_in
+    {
+        public short sin_family;
+        public ushort sin_port;
+        public in_addr sin_addr;
+        public fixed char sin_zero[8];
+    }
+
+    public struct in_addr
+    {
+        public ulong s_addr;
+    }
+
+    /*    public unsafe struct buffer_t{
+            public char[] buffer = new char[2048];
+            public int bufPos;
+        }*/
 
     public unsafe class Connection
     {
 
         public static int MAX_BUF_SIZE = 2048;
-        static ushort PORT_NO = 99999;
+        public static ushort PORT_NO = 9999;
+        public static short AF_INET = 2;
 
-        UInt32 m_rcvAck;
-		UInt32 m_sendAck;
-		UInt32 m_sendSeq;
-		UInt32 m_rcvSeq;
-
+        uint m_ackNo;
         uint m_connectionId;
-        uint m_connectionStatus;
-		UdpAddr m_client;
+        uint m_connectionStatus = 0;
+        sockaddr_in m_in;
+        sockaddr_in m_out;
         uint m_socketId;
         //buffer_t m_buffer;
-        char[] buffer;
-        int bufPos;
+        char[] m_buffer;
+        int m_bufPos;
 
         //fixed char m_buffer[MAX_BUF_SIZE];
         //uint bufPos;
 
-        public Connection(UdpAddr client, uint socketId, uint connectionId)
+        public Connection(sockaddr_in client, sockaddr_in server, uint socketId, uint connectionId)
         {
             m_in = server;
             m_out = client;
             m_socketId = socketId;
             m_connectionId = connectionId;
             m_ackNo = 0;
-            m_connectionStatus = 1;
-            buffer = new char[2048];
+            m_connectionStatus = 0;
+            m_buffer = new char[MAX_BUF_SIZE];
+            m_bufPos = 0;
+            m_ackNo = 0;
+
         }
 
-        public UdpAddr getClient()
+        public sockaddr_in getClient()
         {
             return m_out;
         }
@@ -101,53 +119,64 @@ namespace Connection
 
         public unsafe char[] getBufferAddress()
         {
-            return buffer;
+            return m_buffer;
         }
 
-        public unsafe void write(char[] data)
+
+        public unsafe void ReadFromBuffer(char[] data) //This is now read from buffer. It makes sense, kidn of. It used to be write()
         {
-            if (data.Length + bufPos < MAX_BUF_SIZE)
+            if (data.Length + m_bufPos < MAX_BUF_SIZE)
             {
-                Buffer.BlockCopy(data, 0, buffer, bufPos, data.Length);
+                Buffer.BlockCopy(data, 0, m_buffer, m_bufPos, 2 * data.Length * sizeof(Byte));  //Figure out why the member buffer requires 2B/element
+                m_bufPos += data.Length;
             }
         }
 
-        public void writeToBuffer(char[] otherBuffer)
+        public void WriteToBuffer(char[] otherBuffer)   //This is now write to buffer. 
         {
-            Array.Clear(buffer, 0, buffer.Length);
-            Buffer.BlockCopy(otherbuffer, 0, buffer, 0, otherBuffer.Length);
+            Array.Clear(m_buffer, 0, m_buffer.Length);  //This part doesn't make sense. 
+            Buffer.BlockCopy(otherBuffer, 0, m_buffer, 0, 2 * otherBuffer.Length); //why does this do this
         }
 
-        public void connectionLoop()
+        public void checkSocket()
         {
-            bool connected = true;
-            while (connected)
-            {
-                //Placeholder function call
-                if (pollsocket()) //pollsocket returns true if there is data
-                {
-                  /*
-                  Placeholder function call
-                  Recvfrom should:
-                    receive from socket
-                    write to buffer
-                    (n chars)
-                    store source data in sockaddr
-                  */
-                  recvfrom();
-                }
-
-            }
+            //if (pollsocket()) 
+            //{
+                
+            //}
         }
 
-        public uint getConnectionStatus()
+        //public void connectionLoop()
+        //{
+        //    bool connected = true;
+        //    while (connected)
+        //    {
+        //        //Placeholder function call
+        //        if (pollsocket()) //pollsocket returns true if there is data
+        //        {
+        //            /*
+        //            Placeholder function call
+        //            Recvfrom should:
+        //              receive from socket
+        //              write to buffer
+        //              (n chars)
+        //              store source data in sockaddr
+        //            */
+        //            recvfrom();
+        //        }
+        //    }
+        //}
+
+
+
+        public uint GetConnectionStatus()
         {
             return m_connectionStatus;
         }
 
-        public void setConnectionStatus(uint connectionStatus)
+        public void setConnectionStatus(uint status)
         {
-            m_connectionStatus = connectionStatus;
+            m_connectionStatus = status;
         }
     }
 }
