f23e2c0d0212b09f882f545ce7c0736d8d8f0b5e
angusl2004 <angusl2004@gmail.com>
2018-02-24 09:05:36 -0800

    Created default constants, basic Instantiate(), default constructor, and test script for testing the controller

--- Assets/Scripts/Terrain/TerrainControllerTestScript.cs
+++ Assets/Scripts/Terrain/TerrainControllerTestScript.cs
@@ -1,3 +1,4 @@
+<<<<<<< 131a107a9179aa6c90348664d1212631c1cee1df
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,3 +19,22 @@ public class TerrainControllerTestScript : MonoBehaviour
 
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Assign this to an empty game object in order to test the Terrain Controller
+public class TerrainControllerTestScript : MonoBehaviour {
+
+	// Use this for initialization of the Terrain Object, and for testing function calls
+	void Start () {
+        new TerrainController().Instantiate();
+	}
+	
+	// Unused
+	void Update () {
+		
+	}
+}
+>>>>>>> Created default constants, basic Instantiate(), default constructor, and test script for testing the controller
