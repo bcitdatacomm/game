1a6b1aae1c7ab0b951e4c440453ec69a356844c9
Krixium <bennywang75@hotmail.com>
2018-02-24 10:59:41 -0800

    Fixed coding style of the test script

--- Assets/Scripts/Terrain/TerrainControllerTestScript.cs
+++ Assets/Scripts/Terrain/TerrainControllerTestScript.cs
@@ -1,4 +1,3 @@
-<<<<<<< 131a107a9179aa6c90348664d1212631c1cee1df
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,22 +18,3 @@ public class TerrainControllerTestScript : MonoBehaviour
 
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Assign this to an empty game object in order to test the Terrain Controller
-public class TerrainControllerTestScript : MonoBehaviour {
-
-	// Use this for initialization of the Terrain Object, and for testing function calls
-	void Start () {
-        new TerrainController().Instantiate();
-	}
-	
-	// Unused
-	void Update () {
-		
-	}
-}
->>>>>>> Created default constants, basic Instantiate(), default constructor, and test script for testing the controller
