04a7745a4754a5428458546c1d0f3f2cafee2ad5
MorganAriss <morganariss93@gmail.com>
2018-02-24 15:15:11 -0800

    All views present in main menu; also added login view.

---
+++ Assets/Scripts/TranslateCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateCamera : MonoBehaviour {
+
+	// GameObject camera = GetComponent<GameObject>;
+
+	public void MoveCamera(int y)
+	{
+		gameObject.transform.Translate (0, y, 0);
+	}
+}
