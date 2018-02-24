7b1be04897c3077f826991f59218e78f060529cb
Cooluke75 <luke.lee7575@gmail.com>
2018-02-24 15:19:43 -0800

    Added player spawner sample
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,11 +8,11 @@ public class Bullet : MonoBehaviour {
     {
         var hit = collision.gameObject;
         //var health = hit.GetComponent<Health>();
-        var health = hit.health;
-        if (health != null)
-        {
-            health -= 10;
-        }
+        //var health = hit.health;
+        //if (health != null)
+        //{
+        //    health -= 10;
+        //}
 
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/OtherPlayerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherPlayerSpawner : MonoBehaviour {
+
+    public List<GameObject> otherPlayerList;
+    public GameObject otherPlayerPrefab;
+    public int numberOfPlayers;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // server call to grab otherPlayer data
+        // unpacketize
+	    for (int i = 0; i < numberOfPlayers; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-10.0f, 10.0f), 1.0f, Random.Range(-10.0f, 10.0f));
+            Quaternion spawnRotation = Quaternion.Euler(0.0f, Random.Range(0, 180), 0.0f);
+            GameObject temp = (GameObject)Instantiate(otherPlayerPrefab, spawnPosition, spawnRotation);
+
+            otherPlayerList.Add(temp);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
