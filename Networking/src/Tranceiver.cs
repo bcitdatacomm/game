ed8dbf2ccac5428b7a207ae7a168ab393887420b
lee-jeremy <jeremylee.business@gmail.com>
2018-02-24 19:25:20 -0800

    Redefined and revised data flow in Tranceiver. Still needs details, but data flow is somewhat established.
    Signed-off-by: lee-jeremy <jeremylee.business@gmail.com>

--- Networking/src/Tranceiver.cs
+++ Networking/src/Tranceiver.cs
@@ -1,50 +1,120 @@
 ﻿using System;
+using System.Collections.Concurrent; // CocurrentQueue
+using System.Collections.Generic; // List
 using System.IO; // MemoryStream
 using System.Runtime.Serialization.Formatters.Binary; // BinaryFormatter
+using System.Threading;
 
 namespace Networking
 {
+    /// <summary>
+    /// Driver to send and receive game updates.
+    /// </summary>
     public class Tranceiver
     {
-        private const int BUFFSIZE = 128; // buffer size
+        private const int BUFF_SIZE = 128; // buffer size
+        private const int MAX_CLIENT = 64; // max number of client conns
 
-
+        // Sender and Receiver
+        private Thread thrSender;
+        private Thread thrReceiver;
+        private bool runSender;
+        private bool runReceiver;
+        // data structures
+        private ConcurrentQueue<byte[]> updateQueueToSend;
+        private ConcurrentQueue<byte[]> datagramQueue; // rx datagram queue
+        private List<Connection> connPool; // connection pool
 
         /// <summary>
-        /// Constructor
+        /// Constructor for a Tranceiver object
         /// </summary>
-        public Tranceiver()
+        public Tranceiver(ref List<Connection> connPool, ref ConcurrentQueue<byte[]> datagramQueue)
         {
+            this.runSender = false;
+            this.runReceiver = false;
+            this.updateQueueToSend = new ConcurrentQueue<byte[]>();
+            this.datagramQueue = datagramQueue;
+            this.connPool = connPool;
+        }
 
+        ~Tranceiver()
+        {
+            StopSender();
+            StopReceiver();
+        }
+
+        #region // Sender
+        /// <summary>
+        /// Starts Sender on a separate thread. Use this function to
+        /// start sending queued game updates to all clients.
+        /// </summary>
+        public void StartSender()
+        {
+            this.runSender = true;
+            this.thrSender = new Thread(sendUpdateToClients);
+            this.thrSender.Start();
         }
 
         /// <summary>
-        /// Driver for serializing, encapsulating, and sending a game
-        /// update object to clients
+        /// Stops sender.
+        /// </summary>
+        public void StopSender()
+        {
+            this.runSender = false;
+            Thread.Sleep(1000); // sleep for cleaning up
+            this.thrSender = null;
+        }
+
+        /// <summary>
+        /// Queues a game update object after serializing and encapsulating.
+        /// Use this function to queue a game update object to send to
+        /// clients.
         /// 
         /// TODO: replace gUpdate type with the actual game update type
-        /// TODO: call the low level send function
         /// </summary>
         /// <param name="gUpdate">game update object</param>
-        /// <returns>true if sending a game update is successful</returns>
-        public bool SendUpdate(Object gUpdate)
+        /// <returns>true if queuing is successful</returns>
+        public bool QueueUpdate(Object gUpdate)
         {
-            byte[] buffSnd = new byte[BUFFSIZE]; // buffer for data to send
+            byte[] buffSnd = new byte[BUFF_SIZE]; // buffer for data to send
 
             // serialize game update object
-            buffSnd = SerializeUpdate(gUpdate);
+            buffSnd = serializeUpdate(gUpdate);
             if (buffSnd == null) // SerializeUpdate failed
                 return false;
 
             // encapsulate serialized game update
-            buffSnd = EncapsulateUpdate(buffSnd);
+            buffSnd = encapsulateUpdate(buffSnd);
             if (buffSnd == null) // EncapsulateUpdate failed
                 return false;
 
-            // TODO: send converted game update
+            updateQueueToSend.Enqueue(buffSnd); // queue converted game update
 
+            return true; // Queuing sueccessful
+        }
 
-            return true; // SendUpdate successful
+        /// <summary>
+        /// Driver to send a game update object to all clients. Specify
+        /// the condition of connections to send the game update to.
+        /// 
+        /// TODO: switch the parameter type of write to byte[] in
+        ///       Connection class
+        /// </summary>
+        private void sendUpdateToClients()
+        {
+            while (this.runSender)
+            {
+                if (this.updateQueueToSend.TryDequeue(out byte[] dequeued))
+                {
+                    foreach (Connection conn in this.connPool)
+                    {
+                        if (true) // TODO: specify condition of connection
+                        {
+                            conn.write(dequeued);
+                        }
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -52,9 +122,9 @@ namespace Networking
         /// 
         /// TODO: replace gUpdate type with the actual game update type
         /// </summary>
-        /// <param name="gUpdate"></param>
+        /// <param name="gUpdate">game update object</param>
         /// <returns>serialized game update</returns>
-        private byte[] SerializeUpdate(Object gUpdate)
+        private byte[] serializeUpdate(Object gUpdate)
         {
             if (gUpdate == null) // null check
                 return null;
@@ -73,31 +143,188 @@ namespace Networking
         }
 
         /// <summary>
+        /// Encapsulates the serialized game update.
         /// 
+        /// TODO: encapsulation is not to be implemented for MVP.
         /// </summary>
-        /// <param name="buff"></param>
-        /// <returns>encapsulated, serialized game update</returns>
-        private byte[] EncapsulateUpdate(byte[] buff)
+        /// <param name="buff">serialized game update</param>
+        /// <returns>encapsulated game update</returns>
+        private byte[] encapsulateUpdate(byte[] buff)
         {
-            byte[] buffEncap = new byte[BUFFSIZE];
+            if (buff == null) // null check
+                return null;
+
+            byte[] buffEncap = null;
 
             // TODO: initialize an encoding format
 
             // TODO: convert format of the buffer content
 
+            // TEMP code for MVP
+            buffEncap = buff;
+
             return buffEncap;
+        }
+        #endregion
+
+        #region // Receiver
+        /// <summary>
+        /// Starts the receiver on a separate thread. Use this function
+        /// to start processing received datagrams in datagram queue.
+        /// </summary>
+        public void StartReceiver()
+        {
+            runReceiver = false;
+            thrReceiver = new Thread(receiveDatagramFromClients);
+            thrReceiver.Start();
         }
 
         /// <summary>
-        /// Receives game updates from clients
-        /// 
-        /// TODO: this is a skeleton function. Implement details.
+        /// Stops the receiver thread.
         /// </summary>
-        /// <returns></returns>
-        public bool ReceiveUpdate()
+        public void StopReceiver()
         {
-
-            return true;
+            runReceiver = false;
+            Thread.Sleep(1000); // sleep for cleaning up
+            thrReceiver = null;
         }
+
+        /// <summary>
+        /// Receives datagrams from clients.
+        /// 
+        /// TODO: implement details.
+        /// </summary>
+        private void receiveDatagramFromClients()
+        {
+            int dataType;
+
+            while (runReceiver)
+            {
+                // Receive from all clients
+                if (datagramQueue.TryDequeue(out byte[] datagram))
+                {
+                    // TODO: parse the part that indicates data type from
+                    //       the datagram
+                    //dataType = datagram. ...
+
+                   // TEMP: parsing data type
+                   dataType = 0;
+
+                    switch (dataType)
+                    {
+                        case 0: // TODO: replace the value for Payload
+
+                            // TODO: check status of connection
+                            //       if connecting, change to connected
+
+                            // TODO: update seq
+
+                            // decapsulate the datagram
+                            byte[] buffRx = decapsulateDatagram(datagram);
+                            if (buffRx == null) // decapsulation failed
+                            {
+                                // TODO: log failure
+                                break;
+                            }
+
+                            // deserialize data into game update object
+                            Object gUpdate = deserializeDatagram(buffRx);
+                            if (gUpdate == null) // deserialization failed
+                            {
+                                // TODO: log failure
+                                break;
+                            }
+
+                            // TODO: delegate to game controller
+
+                            break;
+                        case 1: // TODO: replace the value for Connection
+                            // check number of clients
+                            if (connPool.Count < MAX_CLIENT) // not full
+                            {
+                                // TODO: add client to list
+
+                                // TODO: send Challenge packet
+
+                                // TODO: change status of the new client to
+                                //       pending challenge
+                            }
+                            else // capacity full
+                            {
+                                // ignore connection request
+                            }
+
+                            break;
+                        case 2: // TODO: replace the value for Challenging Response
+                            // TODO: check validity of challenge
+                            //       If valid, change connection status
+                            //       to connecting
+
+                            // TODO: send response packet
+                            
+                            // TODO: update seq number
+
+                            break;
+                        case 3: // TODO: replace the value for Keeping Alive
+                            // TODO: check status of connection
+                            //       If connecting, change to connected
+                            //       Else, reset connection timeout and
+                            //       update seq
+
+                            break;
+                        case 4: // TODO: replace the value for Disconnection
+                            // TODO: check if client is currently connected
+                            //       If so, remove the client from pool
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decapsulates received datagram
+        /// </summary>
+        /// <param name="buff">raw datagram</param>
+        /// <returns>decapsulated datagram</returns>
+        private byte[] decapsulateDatagram(byte[] buff)
+        {
+            if (buff == null) // null check
+                return null;
+
+            byte[] buffDecap = null;
+
+            // TODO: initialize a decoder
+
+            // TODO: decapsulate datagram
+
+            // TEMP code for MVP
+            buffDecap = buff;
+
+            return buffDecap;
+        }
+
+        /// <summary>
+        /// Deserializes the decapsulated datagram
+        /// </summary>
+        /// <param name="buff">decapsulated datagram</param>
+        /// <returns>decapsulated game update object</returns>
+        private Object deserializeDatagram(byte[] buff)
+        {
+            if (buff == null)
+                return null;
+
+            Object gUpdate = null;
+
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            ms.Write(buff, 0, buff.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            gUpdate = (Object)bf.Deserialize(ms);
+
+            return gUpdate;
+        }
+        #endregion
     }
 }
