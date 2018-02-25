cbdac38e7562a7c3d0769ac9b41bb968912b6381
clai <clai-@hotmail.com>
2018-02-24 17:31:16 -0800

    server can interact with client and add clients to connection pool

--- Networking/src/server.cpp
+++ Networking/src/server.cpp
@@ -1,9 +1,5 @@
 #include "server.h"
 
-
-
-	
-
 Server::Server()
 	{
 		clientMap = new std::map<int, Connection>();
@@ -13,14 +9,14 @@ Server::Server()
 
 int Server::initializeSocket(short port)
 {
-	if (udpRecvSocket = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0) == -1)
+	int optFlag = 1;
+	if ((udpRecvSocket = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 	{
 		perror("failed to initialize socket");
 		return -1;
 	}
 
-	int optFlag = 1;
-	if(setsockopt(udpRecvSocket, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag) == -1)
+	if (setsockopt(udpRecvSocket, SOL_SOCKET, SO_REUSEADDR, &optFlag, sizeof(int)) == -1)
 	{
 		perror("set opts failed");
 		return -1;
@@ -32,12 +28,12 @@ int Server::initializeSocket(short port)
 
 	int error = -1;
 
-	if((error = bind(udpRecvSocket, (struct  sockaddr *) &serverAddr, sizeof(serverAddr)) == -1)
+	if ((error = bind(udpRecvSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1))
 	{
-		std::cout << "bind error: " errno << std::endl;
+		perror("bind error: ");
 		return error;
 	}
-	
+
 
 
 	return 0;
@@ -49,42 +45,59 @@ int Server::initializeSocket(short port)
 void Server::sendBytes(int clientId, char * data, unsigned len)
 {
 	std::map<int, Connection>::iterator it = clientMap->find(clientId);
-	 
+	const char* buff = "HELLO";
 	if (it != clientMap->end())
 	{
-			
+			socklen_t len = sizeof(it->second.getSocketID());
+			struct sockaddr_in temp = it->second.getClient();
+			sendto(it->second.getSocketID(), buff, 16, 0, (struct sockaddr*)&temp, len);
 	}
 }
 
-
-void initializeConnectionPool()
-{
-	
+sockaddr_in Server::getServerAddr() {
+	return serverAddr;
 }
 
-int32_t UdpRecvFrom(char * buffer, uint32_t size, EndPoint * addr)
+void Server::initializeConnectionPool()
+{
+
+}
+
+bool Server::removeConnection(int socketID)
+{
+	if (clientMap->erase(socketID) == 1) {
+		return true;
+	}
+	return false;
+}
+
+int32_t Server::UdpRecvFrom(char * buffer, uint32_t size, EndPoint * addr)
 {
 	sockaddr_in clientAddr;
-	size_t addrSize = sizeof(clientAddr);
-	
-	int32_t result = recvfrom(udpRecvSocket, buffer, size, 0, (sockaddr *)clientAddr, &addrSize);
+	socklen_t addrSize = sizeof(clientAddr);
 
-	
+	int32_t result = recvfrom(udpRecvSocket, buffer, size, 0, (struct sockaddr *)&clientAddr, &addrSize);
+
+
 	addr->port = ntohs(clientAddr.sin_port);
-	addr->address = ntohl(clientAddr.sin_addr.s_addr);
-
+	addr->addr = ntohl(clientAddr.sin_addr.s_addr);
+	addr->ip.s_addr = clientAddr.sin_addr.s_addr;
+	addr->uport = clientAddr.sin_port;
 	return result;
 }
 
-int32_t  UdpPollSocket()
-{	
+int32_t  Server::UdpPollSocket()
+{
 	int numfds = 1;
 	struct pollfd pollfds;
 	pollfds.fd = udpRecvSocket;
 	pollfds.events = POLLIN;
 
 	int retVal = poll(&pollfds, numfds, 0);
- 	
+ 	if (retVal == -1) {
+		 perror("poll failed with error: ");
+	 }
+
 	if(pollfds.revents & POLLIN)
 	{
 		return SOCKET_DATA_WAITING;
@@ -92,7 +105,7 @@ int32_t  UdpPollSocket()
 
 	return SOCKET_NODATA;
 
-	
+
 }
 
 
@@ -101,13 +114,42 @@ extern "C" Server * Server_CreateServer()
 	return new Server();
 }
 
-extern "C" void Server_sendBytes(void * serverPtr, int clientId, char * data, uint32_t len) 
+extern "C" void Server_sendBytes(void * serverPtr, int clientId, char * data, uint32_t len)
 {
 	((Server *)serverPtr)->sendBytes(clientId, data, len);
 }
 
 extern "C" int32_t Server_recvBytes(void * serverPtr, EndPoint addr, char * buffer, uint32_t * bufSize)
 {
-	int32_t result = (Server *)serverPtr)->UdpRecvFrom(buffer, bufSize, addr);
+
+	int32_t result = static_cast<Server*>(serverPtr)->UdpRecvFrom(buffer, *bufSize, &addr);
 	return result;
+}
+
+int main() {
+	Server* serv = new Server();
+	serv->initializeSocket(5150);
+	int retVal;
+	int sockCount = 100;
+	while (true) {
+		if ((retVal = serv->UdpPollSocket()) == SOCKET_DATA_WAITING) {
+			std::cout << "received data" << std::endl;
+			EndPoint ep;
+			char buff[512];
+			serv->UdpRecvFrom(buff, 512, &ep);
+			std::cout << buff << std::endl;
+			struct sockaddr_in temp;
+			temp.sin_family = AF_INET;
+			temp.sin_port = ep.uport;
+			temp.sin_addr = ep.ip;
+			Connection *c = new Connection(temp, serv->getServerAddr(), sockCount++);
+			std::pair<int, Connection> temp2(c->getSocketID(), *c);
+			serv->clientMap->insert(temp2);
+			serv->removeConnection(102);
+			for (auto it = serv->clientMap->begin(); it != serv->clientMap->end(); ++it) {
+				std::cout << it->first << std::endl;
+			}
+		}
+	}
+	return 1;
 }
--- Networking/src/server.h
+++ Networking/src/server.h
@@ -4,15 +4,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <map>
+#include <poll.h>
+#include <iostream>
 #include "Connection.h"
+#include "Connection.cpp"
+#ifndef SOCK_NONBLOCK
+#include <fcntl.h>
+#define SOCK_NONBLOCK O_NONBLOCK
+#endif
+
+#define SOCKET_NODATA 0
+#define SOCKET_DATA_WAITING 1
 
 struct EndPoint {
 	uint32_t addr;
 	uint16_t port;
+	struct in_addr ip;
+	unsigned short uport;
 };
-
-
-
 
 class Server {
 
@@ -21,10 +30,16 @@ public:
 	int initializeSocket(short port);
 	void sendBytes(int clientId, char * data, unsigned len);
 	std::map<int, Connection> * clientMap;
+	int32_t UdpPollSocket();
+	int32_t UdpRecvFrom(char * buffer, uint32_t size, EndPoint * addr);
+	void initializeConnectionPool();
+	sockaddr_in getServerAddr();
+	bool removeConnection(int socketID);
+
 private:
 	int udpRecvSocket;
 	sockaddr_in serverAddr;
-	struct pollfd* poll_events;	
+	struct pollfd* poll_events;
 
 };
 
