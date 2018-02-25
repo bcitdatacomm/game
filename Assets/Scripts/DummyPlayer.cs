ecdef68826965a377532826cb266f352212559ea
Mackenzie Craig <hello@maccraig.net>
2018-02-25 15:07:54 -0800

    Merging "testsend" feature into team 5 develop as per Benny (#103)
    * Test change (#5)
    
    * added basic pickup template to Vafa's attackable scene
    
    * Stream lined OtherPlayer & FancyPickup into subclassable DummyPlayer & Item classes
    
    * incorporated Luke's attack & bullet code, also added a melee mode toggle on item pickup
    
    * melee bullets can now be available, also added hard-coded playerId
    
    * forgot to remove a sphere collider on dummy player, made hitbox way too big
    
    * Testing Connection.cs class and player spawner object
    
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>
    
    * Added script to main camera so that it follows local player
    
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>
    
    * Bullet physics needs to be modified
    
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>

---
+++ Assets/Scripts/DummyPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayer : Player {
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
