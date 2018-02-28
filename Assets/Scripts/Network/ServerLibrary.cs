46ab5558e5d9124a05ad0bb9aedb527c4d79c019
Delan Elliot <delan.elliot@gmail.com>
2018-02-27 17:17:54 -0800

    add wrappers for client and server

---
+++ Assets/Scripts/Network/ServerLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+
+
+namespace Networking{
+
+
+internal static unsafe class ServerLibrary
+{
+
+	[DllImport("Network.so")]
+	public static extern IntPtr Server_CreateServer();
+
+	[DllImport("Network.so")]
+	public static extern Int32 Server_sendBytes (IntPtr serverPtr, WrapperTest.EndPoint ep, IntPtr buffer, UInt32 len);
+
+	[DllImport("Network.so")]
+	public static extern Int32 Server_recvBytes (IntPtr serverPtr, WrapperTest.EndPoint* ep, IntPtr buffer, UInt32 len);
+
+	[DllImport("Network.so")]
+	public static extern Int32 Server_PollSocket(IntPtr serverPtr);
+
+	[DllImport("Network.so")]
+	public static extern Int32 Server_initServer (IntPtr serverPtr, ushort port);
+
+
+}
+
+}
+
