a501ba0e659cd9189f1bea9b2bee2d1dcc22b221
rogerzhang18 <rogeryj.zhang@gmail.com>
2018-02-28 13:17:27 -0800

    Added map array compression/decompression, changed int array to byte array
    Signed-off-by: rogerzhang18 <rogeryj.zhang@gmail.com>

--- Assets/Scripts/Terrain/TerrainController.cs
+++ Assets/Scripts/Terrain/TerrainController.cs
@@ -229,7 +229,8 @@ public class TerrainController
             }
         }
 
-        this.CompressedData = compressByteArray(compressed.ToArray());
+        this.CompressedData = compressed.ToArray();
+        this.CompressedData = compressByteArray();
     }
 
     /*-------------------------------------------------------------------------------------------------
