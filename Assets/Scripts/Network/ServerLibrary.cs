da75e984af24a4e7cac5b3e636438977bc2989f2
Benny Wang <Krixium@users.noreply.github.com>
2018-02-28 15:46:11 -0800

    Fixed problems where you could not call Networking.so functions from C# (#114)

--- Assets/Scripts/Network/ServerLibrary.cs
+++ Assets/Scripts/Network/ServerLibrary.cs
@@ -10,19 +10,19 @@ namespace Networking{
 internal static unsafe class ServerLibrary
 {
 
-	[DllImport("Network.so")]
+	[DllImport("Network")]
 	public static extern IntPtr Server_CreateServer();
 
-	[DllImport("Network.so")]
-	public static extern Int32 Server_sendBytes (IntPtr serverPtr, WrapperTest.EndPoint ep, IntPtr buffer, UInt32 len);
+	[DllImport("Network")]
+	public static extern Int32 Server_sendBytes (IntPtr serverPtr, EndPoint ep, IntPtr buffer, UInt32 len);
 
-	[DllImport("Network.so")]
-	public static extern Int32 Server_recvBytes (IntPtr serverPtr, WrapperTest.EndPoint* ep, IntPtr buffer, UInt32 len);
+	[DllImport("Network")]
+	public static extern Int32 Server_recvBytes (IntPtr serverPtr, EndPoint* ep, IntPtr buffer, UInt32 len);
 
-	[DllImport("Network.so")]
+	[DllImport("Network")]
 	public static extern Int32 Server_PollSocket(IntPtr serverPtr);
 
-	[DllImport("Network.so")]
+	[DllImport("Network")]
 	public static extern Int32 Server_initServer (IntPtr serverPtr, ushort port);
 
 
