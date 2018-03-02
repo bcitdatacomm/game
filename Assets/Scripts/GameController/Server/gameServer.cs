93157572d42ad6cdda254886b836184a0d305a75
Tim Bruecker <brooker_113@hotmail.com>
2018-03-02 14:59:33 -0800

    New test code

--- Assets/Scripts/GameController/Server/gameServer.cs
+++ Assets/Scripts/GameController/Server/gameServer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using Networking;
  
  
 // This is the core script for the server.
@@ -20,74 +22,78 @@ public class gameServer : MonoBehaviour
    
     // Some example server calls
     private Server server;
-    server.Broadcast(ByteArray);
-    server.SendTo(Server.Connections[0], ByteArray);
+    private EndPoint endpoint;
    
     // Use this for initialization
     void Start ()
     {
         byte playerID = 0;
+        server = new Server();
+        endpoint = new EndPoint("192.168.0.18", 42069);
         terrainController = new TerrainController();
         while (!terrainController.GenerateEncoding());
         TerrainController.Encoding encoded = terrainController.Data;
         int offset = 13;
-       
-       
-        // Make a terrain packet (byte array) with encoded
-        byte[] terrainPacket = new byte[1200];
-       
-        // Set header and amount of packet taken up
-        terrainPacket[0] = 15;
-        int packetSize = 1;
-       
-        // Add data (compressedData should get merged in soon)
-        foreach (byte dataByte in encoded.compressedData)
-        {
-            // Add all the bytes to the packet
-            terrainPacket[packetSize] = dataByte;
- 
-            packetSize += 1;
-           
-            // If we hit the packet size, send the packet and start over
-            if (packetSize == 1200)
-            {
-                // Send the packet THIS NEEDS TO BE RELIABLE (future)
-                server.Broadcast(terrainPacket);
-                packetSize = 1;
-            }
-        }
+
+        server.Init(42069);
+        
+        
+        server.Send(endpoint, Encoding.ASCII.GetBytes("ShitDick"), 8);
+//        // Make a terrain packet (byte array) with encoded
+//        byte[] terrainPacket = new byte[1200];
+//       
+//        // Set header and amount of packet taken up
+//        terrainPacket[0] = 15;
+//        int packetSize = 1;
+//       
+//        // Add data (compressedData should get merged in soon)
+//        foreach (byte dataByte in encoded.compressedData)
+//        {
+//            // Add all the bytes to the packet
+//            terrainPacket[packetSize] = dataByte;
+// 
+//            packetSize += 1;
+//           
+//            // If we hit the packet size, send the packet and start over
+//            if (packetSize == 1200)
+//            {
+//                // Send the packet THIS NEEDS TO BE RELIABLE (future)
+//                server.Broadcast(terrainPacket);
+//                packetSize = 1;
+//            }
+//        }
  
         long tileWidth = this.terrainController.Width;
         long tileLength = this.terrainController.Length;
  
-        for (int i  = 0; i < server.getNumConnections(); i++)
-        {
-            // Sets the coordinates for each player connected
-            float playerX = 0 + playerID;
-            float playerZ = 0 + playerID;
-            float rotation = 0;
- 
-            System.Buffer.BlockCopy(System.BitConverter.GetBytes(playerX), 0, clientData, offset, 4);
-            offset += 4;
-            System.Buffer.BlockCopy(System.BitConverter.GetBytes(playerZ), 0, clientData, offset, 4);
-            offset += 4;
-            System.Buffer.BlockCopy(System.BitConverter.GetBytes(rotation), 0, clientData, offset, 4);
-            offset += 4;
- 
-            playerID++;
-        }
+//        for (int i  = 0; i < server.getNumConnections(); i++)
+//        {
+//            // Sets the coordinates for each player connected
+//            float playerX = 0 + playerID;
+//            float playerZ = 0 + playerID;
+//            float rotation = 0;
+// 
+//            System.Buffer.BlockCopy(System.BitConverter.GetBytes(playerX), 0, clientData, offset, 4);
+//            offset += 4;
+//            System.Buffer.BlockCopy(System.BitConverter.GetBytes(playerZ), 0, clientData, offset, 4);
+//            offset += 4;
+//            System.Buffer.BlockCopy(System.BitConverter.GetBytes(rotation), 0, clientData, offset, 4);
+//            offset += 4;
+// 
+//            playerID++;
+//        }
  
         playerID = 0;
  
-        for (int i = 0; i < server.getNumConnections(); i++)
-        {
-            // Sets the player ID before being sent
-            clientData[372] = playerID;
- 
-            playerID++;
-            // Send each player their spawn location and ID
-            // Need to send clientData here
-        }
+//        for (int i = 0; i < server.getNumConnections(); i++)
+//        {
+//            // Sets the player ID before being sent
+//            clientData[372] = playerID;
+// 
+//            playerID++;
+//            // Send each player their spawn location and ID
+//            // Need to send clientData here
+//        }
  
         // Wait for all IDs to be echoed back as ACK, retransit ID on timeout
         // Set the game timer
@@ -111,7 +117,7 @@ public class gameServer : MonoBehaviour
             // Add all clients coordinates to toBroadcast
  
             // Broadcast update to all connections
-            server.Broadcast(toBroadcast);
+//            server.Broadcast(toBroadcast);
  
  
             // TODO: Bullets
