620a449c6cb4efca56fcb057bbb25b7ed49d4d8d
Tim Bruecker <brooker_113@hotmail.com>
2018-03-02 14:05:46 -0800

    Cleaned up a bit

--- Assets/Scripts/GameController/Server/gameServer.cs
+++ Assets/Scripts/GameController/Server/gameServer.cs
@@ -8,13 +8,20 @@ using UnityEngine;
 
 public class gameServer : MonoBehaviour
 {
-	// Some example server calls
-	 private Server server;
-	 server.Broadcast(ByteArray)
-	 server.SendTo(Server.Connections[0], ByteArray);
+	// Member Data
+	private float nextTickTime = 0.0f;
+	private static int ticksPerSecond = 32;
+	private float tickTime = (1 / ticksPerSecond);
+	private int ticks = 0;
 	
 	private TerrainController terrainController;
 	byte[] clientData = new byte[1200];
+	byte[] toBroadcast = new byte[1200];
+	
+	// Some example server calls
+	private Server server;
+	server.Broadcast(ByteArray);
+	server.SendTo(Server.Connections[0], ByteArray);
 	
 	// Use this for initialization
 	void Start () 
@@ -87,39 +94,6 @@ public class gameServer : MonoBehaviour
 		// Start the game timer
 
 	}
-	
-	// Update is called once per frame
-	//void Update () {
-//		Receive data from each client
-//		Update local data		
-//		Check for bullet - player collisions
-//		for(bullet in bullets) {
-//			for(player in players) {
-//				//copied http://cgp.wikidot.com/circle-to-circle-collision-detection
-//				//compare the distance to combined radii
-//				int dx = x2 - x1;
-//				int dy = y2 - y1;
-//				int radii = radius1 + radius2;
-//				if ( ( dx * dx )  + ( dy * dy ) < radii * radii ) 
-//				{
-//					Console.WriteLine("The 2 circles are colliding!");
-//				}
-//			}
-//		}
-//
-//		Apply control input to player coordinates
-//	
-//	
-//		Send updated data (either one tick packet, or a tick packet and a bullet packet) to each player
-//
-	//}
-
-    private float nextTickTime = 0.0f;
-    private static int ticksPerSecond = 32;
-    private float tickTime = (1 / ticksPerSecond);
-    private int ticks = 0;
-
-    byte[] toBroadcast = new byte[1200];
 
     void Update()
     {
@@ -155,7 +129,6 @@ public class gameServer : MonoBehaviour
             //          }
             //      }
 
-            // TODO: Unit collision
         }
     }
 }
