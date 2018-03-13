cbb6446e97809c8c26e784b3cc9eb3a71851317e
Alfred Swinton <alfredswinton@gmail.com>
2018-03-12 18:26:37 -0700

    Added R.cs Constant file to Assets/Scripts

---
+++ Assets/Scripts/R.cs
@@ -0,0 +1,43 @@
+﻿namespace R
+{
+    // Contains all the constants associated with networking and the packet
+    public static class Net
+    {
+        // Contains constants associated with the header type of the packet
+        public static class Header
+        {
+
+        }
+
+        // Contains constants associated with the packet offset or distance into the packet
+        public static class Offset
+        {
+            // Description outlining the constant
+            public static int ExampleOffsetConstant = 0;
+        }
+
+        // Contains related to size of the data being sent
+        public static class Size
+        {
+            // Description outlining the constant
+            public static int ExampleSizeConstant = 1;
+        }
+
+    }
+
+    // Contains Constants Related to the game
+    public static class Game
+    {
+        // Terrain Constants
+        public static class Terrain
+        {
+
+        }
+
+        // Player Constants
+        public static class Players
+        {
+
+        }
+    }
+}
