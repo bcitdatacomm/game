ffb83167817e0ea8afc8e93c2222c817ba494204
lee-jeremy <jeremylee.business@gmail.com>
2018-03-13 12:29:50 -0700

    Select added on network portion (UDP)
    Signed-off-by: lee-jeremy <jeremylee.business@gmail.com>

--- Networking/src/library.h
+++ Networking/src/library.h
@@ -1,10 +1,12 @@
 #include "server.h"
 
-extern "C" Server * Server_CreateServer();
+extern "C" Server *Server_CreateServer();
 
 extern "C" int32_t Server_initServer(void *);
 
 extern "C" int32_t Server_PollSocket(void *);
+
+extern "C" int32_t Server_SelectSocket(void *);
 
 extern "C" int32_t Server_sendBytes(void *, EndPoint, char *, uint32_t len);
 
