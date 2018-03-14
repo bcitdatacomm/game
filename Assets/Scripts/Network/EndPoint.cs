e0feef4ec6529471e90ccae92528a8269e81a79b
Delan Elliot <delan.elliot@gmail.com>
2018-03-14 16:54:09 -0700

    Git ignore network.so

--- Assets/Scripts/Network/EndPoint.cs
+++ Assets/Scripts/Network/EndPoint.cs
@@ -3,52 +3,52 @@ using System.Runtime.InteropServices;
 
 namespace Networking
 {
-	[StructLayout(LayoutKind.Explicit, Pack = 1)]
-	public struct CAddr{
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public struct CAddr{
 
-		public static readonly CAddr Loopback = new CAddr("127.0.0.1");
+        public static readonly CAddr Loopback = new CAddr("127.0.0.1");
 
-		[FieldOffset(0)]
-		public readonly uint Packet;
-		[FieldOffset(0)]
-		public readonly byte Byte0;
-		[FieldOffset(1)]
-		public readonly byte Byte1;
-		[FieldOffset(2)]
-		public readonly byte Byte2;
-		[FieldOffset(3)]
-		public readonly byte Byte3;
-
-
-		public CAddr (string ip) {
-			string[] parts = ip.Split('.');
-			Packet = 0; 
-			Byte0 = byte.Parse(parts[3]);
-			Byte1 = byte.Parse(parts[2]);
-			Byte2 = byte.Parse(parts[1]);
-			Byte3 = byte.Parse(parts[0]);
-		}
-
-		public CAddr (byte a, byte b, byte c, byte d) {
-			Packet = 0;
-			Byte0 = d;
-			Byte1 = c;
-			Byte2 = b;
-			Byte3 = a;
-		}
-
-	}
-
-	[StructLayout(LayoutKind.Sequential, Pack = 1)]
-	public struct EndPoint {
-		public readonly CAddr addr;
-		public readonly ushort port;
+        [FieldOffset(0)]
+        public readonly uint Packet;
+        [FieldOffset(0)]
+        public readonly byte Byte0;
+        [FieldOffset(1)]
+        public readonly byte Byte1;
+        [FieldOffset(2)]
+        public readonly byte Byte2;
+        [FieldOffset(3)]
+        public readonly byte Byte3;
 
 
-		public EndPoint(string ipAddr, ushort p)
-		{
-			addr = new CAddr (ipAddr);
-			port = p;
+        public CAddr (string ip) {
+            string[] parts = ip.Split('.');
+            Packet = 0; 
+            Byte0 = byte.Parse(parts[3]);
+            Byte1 = byte.Parse(parts[2]);
+            Byte2 = byte.Parse(parts[1]);
+            Byte3 = byte.Parse(parts[0]);
+        }
+
+        public CAddr (byte a, byte b, byte c, byte d) {
+            Packet = 0;
+            Byte0 = d;
+            Byte1 = c;
+            Byte2 = b;
+            Byte3 = a;
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct EndPoint {
+        public readonly CAddr addr;
+        public readonly ushort port;
+
+
+        public EndPoint(string ipAddr, ushort p)
+        {
+            addr = new CAddr (ipAddr);
+            port = p;
         }
 
         public override int GetHashCode ()
@@ -56,13 +56,13 @@ namespace Networking
             return (int) (CAddr.Packet ^ Port);
         }
 
-		public override string ToString ()
-		{
-			return string.Format ("{0}.{1}.{2}.{3}:{4}", addr.Byte3, addr.Byte2, addr.Byte1, addr.Byte0, port);
-		}
+        public override string ToString ()
+        {
+            return string.Format ("{0}.{1}.{2}.{3}:{4}", addr.Byte3, addr.Byte2, addr.Byte1, addr.Byte0, port);
+        }
 
-		 public static bool operator == (EndPoint x, EndPoint y)
-		{
+        public static bool operator == (EndPoint x, EndPoint y)
+        {
             return Compare(x, y) == 0;
         }
 
@@ -81,7 +81,7 @@ namespace Networking
 
             return 0;
         }
-	}
+    }
 
     public class EndPointComparer : IEqualityComparer<EndPoint> {
         bool IEqualityComparer<EndPoint>.Equals (EndPoint x, EndPoint y) {
