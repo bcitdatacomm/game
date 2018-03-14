e194e08a2d5342f51a230c28b1b2d91ae31116d9
Krixium <bennywang75@hotmail.com>
2018-03-14 16:22:40 -0700

    Squashing changes for rebasing

---
+++ Assets/Scripts/GameController/Client/GameController.cs
@@ -0,0 +1,228 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Networking;
+
+public class GameController : MonoBehaviour {
+
+    public const string SERVER_ADDRESS = "192.168.0.20";
+    public const ushort SERVER_PORT = 42069;
+    public const int PACKET_SIZE = 1200;
+    public const byte INIT_HEADER = 0;
+    public const byte TICK_HEADER = 85;
+    public const byte ACK_HEADER = 170;
+    public const int ID_OFFSET = 373;
+    public const int POSITION_OFFSET = 13;
+    public const int ROTATION_OFFSET = 253;
+
+    private byte currentPlayerId;
+
+    private Dictionary<byte, GameObject> players;
+
+    byte[] buffer;
+    private Client client;
+
+    public GameObject PlayerPrefab;
+    public GameObject EnemyPrefab;
+
+    void Start()
+    {
+        currentPlayerId = 0;
+        players = new Dictionary<byte, GameObject>();
+
+        buffer = new byte[PACKET_SIZE];
+        client = new Client();
+        client.Init(SERVER_ADDRESS, SERVER_PORT);
+
+        byte[] initPacket = new byte[PACKET_SIZE];
+        initPacket[0] = 69;
+
+        client.Send(initPacket, PACKET_SIZE);
+    }
+
+    void FixedUpdate()
+    {
+        if (this.currentPlayerId != 0)
+        {
+            this.updateGameState();
+        }
+        else
+        {
+            this.syncWithServer();
+        }
+    }
+
+    void updateGameState()
+    {
+        this.sendPlayerDataToServer();
+
+        if (client.Poll() == Client.SOCKET_NO_DATA)
+        {
+            return;
+        }
+
+        if (client.Recv(buffer, PACKET_SIZE) < PACKET_SIZE)
+        {
+            return;
+        }
+
+        List<byte> playerIDs = this.getPlayerIDs(buffer);
+        List<Vector3> positions = this.getPlayerPositions(buffer);
+        List<Quaternion> rotations = this.getPlayerRotations(buffer);
+
+        // Add any new players
+        if (playerIDs.Count > this.players.Count)
+        {
+            for (int i = 0; i < playerIDs.Count; i++)
+            {
+                if (this.players.ContainsKey(playerIDs[i]))
+                {
+                    continue;
+                }
+                this.addPlayer(playerIDs[i], positions[i], rotations[i]);
+            }
+        }
+
+        this.movePlayers(playerIDs, positions, rotations);
+    }
+
+    void syncWithServer()
+    {
+        if (this.client.Poll() == Client.SOCKET_NO_DATA)
+        {
+            return;
+        }
+
+        if (this.client.Recv(buffer, PACKET_SIZE) < PACKET_SIZE)
+        {
+            return;
+        }
+
+        if (this.buffer[0] != INIT_HEADER)
+        {
+            return;
+        }
+        this.currentPlayerId = buffer[ID_OFFSET];
+        this.addPlayer(this.currentPlayerId, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+    }
+
+    List<byte> getPlayerIDs(byte[] data)
+    {
+        List<byte> playerIDs = new List<byte>();
+        for (int i = 0; i < 30; i++)
+        {
+            byte id = data[ID_OFFSET + i];
+
+            if (id == 0)
+            {
+                return playerIDs;
+            }
+
+            playerIDs.Add(id);
+        }
+        return playerIDs;
+    }
+
+    List<Vector3> getPlayerPositions(byte[] data)
+    {
+        List<Vector3> positions = new List<Vector3>();
+        for (int i = 0; i < 30; i++)
+        {
+            float x = BitConverter.ToSingle(data, POSITION_OFFSET + (i * 8));
+            float z = BitConverter.ToSingle(data, POSITION_OFFSET + (i * 8) + 4);
+            Vector3 position = new Vector3(x, 0, z);
+            positions.Add(position);
+        }
+        return positions;
+    }
+
+    List<Quaternion> getPlayerRotations(byte[] data)
+    {
+        List<Quaternion> rotations = new List<Quaternion>();
+        for (int i = 0; i < 30; i++)
+        {
+            float pheta = BitConverter.ToSingle(data, ROTATION_OFFSET + (i * 4));
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, pheta, 0));
+            rotations.Add(rotation);
+        }
+        return rotations;
+    }
+
+    void addPlayer(byte id, Vector3 position, Quaternion rotation)
+    {
+        GameObject player;
+
+        if (id == this.currentPlayerId)
+        {
+            player = (GameObject)Instantiate(this.PlayerPrefab, position, rotation);
+        }
+        else
+        {
+            player = (GameObject)Instantiate(this.EnemyPrefab, position, rotation);
+        }
+
+        this.players.Add(id, player);
+    }
+
+    void movePlayers(List<byte> playerIDs, List<Vector3> positions, List<Quaternion> rotations)
+    {
+        try
+        {
+            for (int i = 0; i < playerIDs.Count; i++)
+            {
+                if (playerIDs[i] == this.currentPlayerId)
+                {
+                    continue;
+                }
+                Debug.Log("Applying position : " + positions[i] + " and rotation : " + rotations[i] + " to id : " + playerIDs[i]);
+                this.players[playerIDs[i]].transform.position = positions[i];
+                this.players[playerIDs[i]].transform.rotation = rotations[i];
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    void sendPlayerDataToServer()
+    {
+        int index = 2;
+        GameObject currentPlayer = this.players[this.currentPlayerId];
+        byte[] x = BitConverter.GetBytes(currentPlayer.transform.position.x);
+        byte[] z = BitConverter.GetBytes(currentPlayer.transform.position.z);
+        byte[] pheta = BitConverter.GetBytes(currentPlayer.transform.rotation.y);
+        byte[] packet = new byte[PACKET_SIZE];
+
+        // Put position data into the packet
+        packet[0] = TICK_HEADER;
+        packet[1] = this.currentPlayerId;
+        Array.Copy(x    , 0, packet, index,  4);
+        index += 4;
+        Array.Copy(z    , 0, packet, index,  4);
+        index += 4;
+        Array.Copy(pheta, 0, packet, index, 4);
+        index += 4;
+
+        /*
+        // The reference to the stack of fired bullets does not work
+
+        Stack<Bullet> playerBullets = this.players[this.currentPlayerId].GetComponent<Gun>().FiredShots;
+
+        while (playerBullets.Count > 0)
+        {
+            Bullet bullet = playerBullets.Pop();
+            byte[] bulletID = BitConverter.GetBytes(bullet.ID);
+
+            Array.Copy(bulletID, 0, packet, index, 4);
+            index += 4;
+
+            packet[index] = bullet.Type;
+        }
+        */
+
+        Debug.Log(this.client.Send(packet, PACKET_SIZE));
+    }
+}
--- Assets/Scripts/GameController/Server/gameServer.cs
+++ Assets/Scripts/GameController/Server/gameServer.cs
@@ -1,19 +1,265 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
-
+using Networking;
+using System.Threading;
 
 // This is the core script for the server.
 
-public class gameServer : MonoBehaviour {
+public unsafe class gameServer : MonoBehaviour
+{
+    // Member Data
+    private static float nextTickTime = 0.0f;
+    private static int ticksPerSecond = 32;
+    private float tickTime = (1 / ticksPerSecond);
+    private int ticks = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static Int32 SOCKET_NODATA = 0;
+    private static Int32 SOCKET_DATA_WAITING = 1;
+    private static ushort PORT_NO = 9999;
+    private static int MAX_BUFFER_SIZE = 1200;
+    private static Server server;
+    private static IntPtr serverInstance;
+    private static bool running;
+    private static Thread recvThread;
+
+    private TerrainController terrainController;
+    private static byte[] clientData = new byte[MAX_BUFFER_SIZE];
+
+    private static List<connection> endpoints;
+    public struct connection
+    {
+        public EndPoint ep;
+        public byte[] buffer;
+        public byte id;
+
+        public float x;
+        public float z;
+        public float r;
+
+        public int h;
+    }
+    static byte playerID = 1;
+    static float spawnPoint = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        server = new Server();
+        terrainController = new TerrainController();
+        while (!terrainController.GenerateEncoding()) ;
+        TerrainController.Encoding encoded = terrainController.Data;
+
+        server.Init(42069);
+
+        endpoints = new List<connection>();
+        recvThread = new Thread(recvThrdFunc);
+        running = true;
+        recvThread.Start();
+
+        /*
+         TODO: Implement this in TCP
+        // Make a terrain packet (byte array) with encoded
+        byte[] terrainPacket = new byte[1200];endpoints[i]
+
+        // Set header and amount of packet taken up
+        terrainPacket[0] = 15;
+        int packetSize = 1;
+
+        long tileWidth = this.terrainController.Width;
+        long tileLength = this.terrainController.Length;
+
+        // Wait for all IDs to be echoed back as ACK, retransit ID on timeout
+        */
+
+        // Set the game timer
+        // Start the game timer
+
+    }
+
+    void Update()
+    {
+        if (Time.time > nextTickTime)
+        {
+            ticks++;
+            nextTickTime += tickTime;
+
+            craftTickPacket();
+            sendPacketToClients();
+        }
+    }
+
+    private static void craftTickPacket()
+    {
+        int offset = 373;
+        clientData[0] = 85;
+
+        for (int i = 0; i < endpoints.Count; i++)
+        {
+            connection conn = endpoints[i];
+
+            // New connection
+            if (conn.id == 0 && playerID < 30)
+            {
+                conn.id = playerID;
+                playerID++;
+                sendInitData(ref conn);
+            }
+
+            // Update clientdata with new coordinates
+            if (conn.buffer != null)
+            {
+                updateCoord(ref conn);
+                conn.buffer = null;
+            }
+
+            endpoints[i] = conn;
+
+            // Add player id to clientdata
+            clientData[offset] = conn.id;
+            offset++;
+        }
+    }
+
+    private static void sendPacketToClients()
+    {
+        Debug.Log("send | " + byteArrayToString(clientData));
+        for (int i = 0; i < endpoints.Count; i++)
+        {
+            if (endpoints[i].id != 0)
+            {
+                server.Send(endpoints[i].ep, clientData, MAX_BUFFER_SIZE);
+            }
+        }
+    }
+
+    public static void recvThrdFunc()
+    {
+
+        byte[] recvBuffer = new byte[MAX_BUFFER_SIZE];
+        Int32 numRead;
+        EndPoint ep = new EndPoint();
+
+        while (running)
+        {
+            // If poll returns 1 (SOCKET_DATA_WAITING), there is data waiting to be read
+            // If poll returns 0 (SOCKET_NODATA), there is no data waiting to be read
+            if (server.Poll() == SOCKET_DATA_WAITING)
+            {
+                connection recvConn = new connection();
+                numRead = server.Recv(&ep, recvBuffer, MAX_BUFFER_SIZE);
+
+                if (numRead != MAX_BUFFER_SIZE)
+                {
+                    Debug.Log("Failed to read from socket.");
+                }
+                else
+                {
+                    Debug.Log("recv | " + byteArrayToString(recvBuffer));
+                    switch (recvBuffer[0])
+                    {
+                        case 69:
+                            addNewClient(ep);
+                            break;
+                        
+                        case 85:
+                            saveBuffer(ep, recvBuffer);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void addNewClient(EndPoint ep)
+    {
+        connection newPlayer = new connection();
+        
+        if (playerID < 31)
+        {
+            newPlayer.ep = ep;
+            newPlayer.id = 0;
+
+            endpoints.Add(newPlayer);
+        }
+    }
+
+    private static void saveBuffer(EndPoint ep, byte[] buffer)
+    {
+        for (int i = 0; i < endpoints.Count; i++)
+        {
+            if (endpoints[i].id == buffer[1])
+            {
+                if (ep.addr.Byte0 == endpoints[i].ep.addr.Byte0 && ep.addr.Byte1 == endpoints[i].ep.addr.Byte1
+                    && ep.addr.Byte2 == endpoints[i].ep.addr.Byte2 && ep.addr.Byte3 == endpoints[i].ep.addr.Byte3)
+                {
+                    connection tmp = endpoints[i];
+                    tmp.buffer = buffer;
+                    endpoints[i] = tmp;
+                }
+            }
+        }
+    }
+
+    //Creates a new player's information
+    private static void sendInitData(ref connection conn)
+    {
+        clientData[0] = 0;
+        clientData[373] = conn.id;
+
+        int positionOffset = (13 + (conn.id * 8)) - 8;
+        int rotationOffset = (253 + (conn.id * 4)) - 4;
+
+        conn.x = spawnPoint * 5;
+        conn.z = spawnPoint * 5;
+        spawnPoint *= spawnPoint;
+        conn.r = 0.0f;
+        conn.h = 100;
+
+        Array.Copy(BitConverter.GetBytes(conn.x), 0, clientData, positionOffset, 4);
+        Array.Copy(BitConverter.GetBytes(conn.z), 0, clientData, positionOffset + 4, 4);
+        Array.Copy(BitConverter.GetBytes(conn.r), 0, clientData, rotationOffset, 4);
+
+        server.Send(conn.ep, clientData, MAX_BUFFER_SIZE);
+    }
+
+    // Takes the recieved coords and updates client data
+    private static void updateCoord(ref connection conn)
+    {
+        if (!conn.buffer[0].Equals(85))
+        {
+            return;
+        }
+
+        if (conn.buffer[1] != conn.id)
+        {
+            return;
+        }
+
+        int positionOffset = (13 + (conn.id * 8)) - 8;
+        int rotationOffset = (253 + (conn.id * 4)) - 4;
+
+        Array.Copy(conn.buffer, 2, clientData, positionOffset, 4);
+        Array.Copy(conn.buffer, 6, clientData, positionOffset + 4, 4);
+        Array.Copy(conn.buffer, 10, clientData, rotationOffset, 4);
+
+        conn.x = BitConverter.ToSingle(conn.buffer, 2);
+        conn.z = BitConverter.ToSingle(conn.buffer, 6);
+        conn.r = BitConverter.ToSingle(conn.buffer, 10);
+    }
+
+    static string byteArrayToString(byte[] ba)
+    {
+        StringBuilder sb = new StringBuilder(ba.Length * 2);
+        foreach (byte b in ba)
+        {
+            sb.AppendFormat("{0:x2}", b);
+        }
+        return sb.ToString();
+    }
 }
