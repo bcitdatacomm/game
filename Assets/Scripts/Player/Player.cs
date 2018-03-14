6e2edcaeeb8382630ed25226f5a6ad1ce5d163f5
Tliyen <liyan.tong@gmail.com>
2018-03-14 12:57:45 -0700

    Set up initial player movement and bullet generation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,32 +6,88 @@ public class Player : MonoBehaviour
 {
     public int Heath;
 
+    public int Armor;
+
     public float MovementSpeed;
 
     public Spell[] Spells;
+
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 
     void Start()
     {
         Debug.Log("Player start");
         this.Heath = 100;
+        this.Heath = 0;
         this.Spells = new Spell[3];
+    }
+
+    void Awake()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("Floor");
+
+        // Set up references.
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        this.move();
-        this.turn();
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        move(h, v);
+
+        // Turn the player to face the mouse cursor.
+        turn();
     }
 
-    void move()
+    void move(float h, float v)
     {
-        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
-        float deltaZ = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
-        this.transform.Translate(deltaX, 0, deltaZ);
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * MovementSpeed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
     }
 
     void turn()
     {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //transform.LookAt(ray.GetPoint(0), Vector3.forward);
+
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
     }
 }
