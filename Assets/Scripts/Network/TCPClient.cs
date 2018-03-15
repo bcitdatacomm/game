b244fc7b0ad3f22406141a5bf90cff93da39ca00
Krixium <bennywang75@hotmail.com>
2018-03-14 20:16:30 -0700

    Refactored packet sizes and offsets to use constants and tweaked their values

--- Assets/Scripts/Network/TCPClient.cs
+++ Assets/Scripts/Network/TCPClient.cs
@@ -11,26 +11,32 @@ namespace Networking
 			tcpClient = ServerLibrary.TCPClient_CreateClient();
 		}
 
-		public Int32 Send(byte[] buffer, Int32 len)
-		{
-			fixed (byte* tmpBuf = buffer)
-			{
-				UInt32 bufLen = Convert.ToUInt32 (len);
-				Int32 ret = ServerLibrary.TCPClient_sendBytes(tcpClient, new IntPtr(tmpBuf), bufLen);
-				return ret;
-			}
+        public Int32 Send(byte[] buffer, Int32 len)
+        {
+            unsafe
+            { 
+                fixed (byte* tmpBuf = buffer)
+                {
+                    UInt32 bufLen = Convert.ToUInt32(len);
+                    Int32 ret = ServerLibrary.TCPClient_sendBytes(tcpClient, new IntPtr(tmpBuf), bufLen);
+                    return ret;
+                }
+            }
 		}
 
 		public Int32 Recv(byte[] buffer, Int32 len)
 		{
 			Int32 length;
-			fixed (byte* tmpBuf = buffer)
-			{
-				UInt32 bufLen = Convert.ToUInt32(len);
-				length = ServerLibrary.TCPClient_recvBytes(tcpClient, new IntPtr(tmpBuf), bufLen);
+            unsafe
+            {
+                fixed (byte* tmpBuf = buffer)
+                {
+                    UInt32 bufLen = Convert.ToUInt32(len);
+                    length = ServerLibrary.TCPClient_recvBytes(tcpClient, new IntPtr(tmpBuf), bufLen);
 
-				return length;
-			}
+                    return length;
+                }
+            }
 
 		}
 
