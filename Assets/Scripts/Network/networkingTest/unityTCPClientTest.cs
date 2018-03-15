c4a23c81900c730ebf0a28823ceed376e4d9f92b
wozenwho <wilson.huu@gmail.com>
2018-03-14 18:23:34 -0700

    Added unity TCP tests pls work

---
+++ Assets/Scripts/Network/networkingTest/unityTCPClientTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Networking;
+
+
+/*
+* This is the client version of the networking test program.
+*
+* To use this, create a Server prefab and attach this file as a script.
+* This test works with an echo-server implementation that is also run in Unity.
+*
+* It creates a single thread to read the client-EndPoint (socket) and
+* prints the received buffer to the Unity console.
+*
+* It sends a 600B packet every time Update is called.
+*/
+public class unityTCPClientTest : MonoBehaviour {
+
+	public static int SOCKET_NODATA 		= 0;
+	public static int SOCKET_DATA_WAITING 	= 1;
+	private static int MAX_BUFFER_SIZE 		= 8192;
+	private static string destIP 			= "192.168.0.13";
+	private ushort portNo	 				= 9999;
+	private EndPoint ep;
+	private static bool running;
+	byte i									= 65;
+
+	private TCPClient client;
+
+	/*
+    * Start initializes the Client object and recvThread.
+    */
+	void Start () {
+		Debug.Log ("Start Client-Send test");
+		Int32 result;
+		byte[] sendBuffer = new Byte[MAX_BUFFER_SIZE];
+
+		Thread recvThread;
+
+		client = new TCPClient ();
+		ep = new EndPoint (destIP, portNo);
+		client.Init (ep);
+
+		// Starts the recv thread (listens for the echo)
+		running = true;
+		recvThread = new Thread (recvThrdFunc);
+		recvThread.Start();
+
+	} // End of Start()
+
+	// Update is called once per frame
+	void Update () {
+		byte[] sendBuffer = new Byte[MAX_BUFFER_SIZE];
+		int numSent;
+
+		// reset i to avoid byte overflow
+		// i = 0 - 9
+		if (i > 57)
+		{
+			i = 48;
+		}
+
+		// Write 600 of the current char to the byte array
+		for (int j = 0; j < MAX_BUFFER_SIZE; j++)
+		{
+			sendBuffer [j] = i;
+		}
+
+		// Send data to server
+		numSent = client.Send (sendBuffer, MAX_BUFFER_SIZE);
+
+		if (numSent <= 0)
+		{
+			Debug.Log ("Send fail.");
+		}
+		Debug.Log ("Sent");
+		i++;
+
+	} // End of Update()
+
+
+	// Terminate thread when we stop running.
+	private void OnDisable()
+	{
+		Debug.Log ("DISABLED");
+		running = false;
+	}
+
+	/*
+    * Thread function to read data from the EndPoint.
+    * Prints the received buffer to the Unity console.
+    */
+	private void recvThrdFunc()
+	{
+		byte[] recvBuffer = new byte[MAX_BUFFER_SIZE];
+		Int32 numRead;
+		UInt64 totalRead = 0;
+		int i = 0;
+		int numPollPass = 0;
+		int numPollFail = 0;
+
+		Debug.Log ("recvThread started.");
+
+		while (running)
+		{
+			client.Recv (recvBuffer, MAX_BUFFER_SIZE);
+		}
+	}
+}
---
+++ Assets/Scripts/Network/networkingTest/unityTCPEchoServer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Networking;
+
+/**
+ * Simple echo server implementation using our Networking library.
+ * This class is used as a Unity script which can be attached to a prefab for 
+ * testing. (I used Server)
+ * 
+ * Throughput seems to be limited by Client's Update rate, ~200 updates sent/received per second 
+ */
+public unsafe class unityTCPEchoServer : MonoBehaviour {
+
+	private static int MAX_BUFFER_SIZE = 8192;
+	public static int SOCKET_NODATA = 0;
+	public static int SOCKET_DATA_WAITING = 1;
+
+	private TCPServer server;
+	private ushort portNo = 9999;
+	private bool running;
+	private EndPoint ep;
+
+
+	/*
+	 * Creates and initializes the Server object and recv thread.
+	 */ 
+	void Start ()
+	{
+		Debug.Log("Starting Server-Echo test");
+
+		// Creates a blank EndPoint which will be filled by the Server.Recv call.
+		ep = new EndPoint();
+		Thread recvThread;
+		Int32 result;
+
+		// Create and initialize a server.
+		server = new TCPServer();
+		result = server.Init(portNo);
+		if (result != 0)
+		{
+			Debug.Log("Failed to initialize socket");
+		}
+			
+		recvThread = new Thread(recvThrdFunc);
+		running = true;
+		recvThread.Start();
+
+	} // End of Start()
+
+
+	//Terminate thread when we stop running.
+	private void OnDisable()
+	{
+		Debug.Log("DISABLED.");
+		running = false;
+	}
+
+	/*
+     * Thread function to read incoming packets.
+     * 
+     * Currently works when tested using lab computers.
+     * Initially, the server would recv only the first packet. 
+     * 
+     * Sleep call was removed, with successful recv on the server side.
+     *
+     */
+	private void recvThrdFunc()
+	{
+		bool result;
+		Int32 totalRecv = 0;
+		Int32 numRecvPass = 0;
+		Int32 numPollFail = 0;
+		Int32 numSent = 0;
+		Int32 numBytesSent;
+		Int32 client;
+
+		byte[] recvBuffer = new byte[MAX_BUFFER_SIZE];
+		Int32 numRecv;
+
+		while (running)
+		{
+			
+			client = server.AcceptConnection (ref ep);
+			if (client == -1)
+			{
+				Debug.Log ("Fuck I couldn't accept fuck.");
+			}
+			server.Recv (client, recvBuffer, MAX_BUFFER_SIZE);
+			string contents = System.Text.Encoding.UTF8.GetString (recvBuffer);
+			Debug.Log ("Received: " + contents);
+			numSent = server.Send (client, recvBuffer, MAX_BUFFER_SIZE);
+			if (numSent > 0)
+			{
+				Debug.Log ("Sent.");
+			}
+
+		}
+	} // End of recvThrdFunc()
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
