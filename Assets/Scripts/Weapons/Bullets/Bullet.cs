00fc62577b4e69af735882735df7ae94c5df3fbe
Tliyen <liyan.tong@gmail.com>
2018-03-15 18:34:12 -0700

    Update Weapon Pre-fabs & scripts

--- Assets/Scripts/Weapons/Bullets/Bullet.cs
+++ Assets/Scripts/Weapons/Bullets/Bullet.cs
@@ -14,6 +14,8 @@ public class Bullet : MonoBehaviour {
     void Start()
     {
         this.ID = GetInstanceID();
+
+        // TODO: Destroy object after LifeTime is up
     }
 
     void FixedUpdate()
