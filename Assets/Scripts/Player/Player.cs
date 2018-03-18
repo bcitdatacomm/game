a1f98d4c8daec2844cfcbcd4259b9b307580de5f
anthony vu <anthony_vu@live.ca>
2018-03-18 14:51:25 -0700

    Set up base for adding audio

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     Vector3 movement;                   // The vector to store the direction of the player's movement.
     Animator anim;                      // Reference to the animator component.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    AudioSource backgroundMusic;
     int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer
     float camRayLength = 100f;          // The length of the ray from the camera into the scene.
     public Vector3 net;
@@ -31,6 +32,8 @@ public class Player : MonoBehaviour
         net = Vector3.zero;
         MovementSpeed = .1f;
 
+        backgroundMusic = GetComponent<AudioSource>();
+        backgroundMusic.Play();
         // TEST CODE
         // GameObject Pistol = Instantiate(Resources.Load("Pistol", typeof(GameObject))) as GameObject;
         // currentGun = Pistol.GetComponent("Gun") as Gun;
