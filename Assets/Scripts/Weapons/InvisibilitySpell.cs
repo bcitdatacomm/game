b31d23349308b242e838de4b570f6faa6257d86e
jeff <jc@gmail.com>
2018-03-18 14:37:44 -0700

    added telport spell and invisibility spell

---
+++ Assets/Scripts/Weapons/InvisibilitySpell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibilitySpell : Spell {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire2"))
+        {
+			this.transform.parent.Find("char02").GetComponent<Renderer>().enabled = false;
+        }
+		if(Input.GetButton("Fire1"))
+        {
+			this.transform.parent.Find("char02").GetComponent<Renderer>().enabled = true;
+        }
+	}
+}
---
+++ Assets/Scripts/Weapons/TeleportSpell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportSpell : Spell {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire2"))
+        {
+
+			float randomDistance = 25f;
+			Vector3 teleportVector = new Vector3(Random.Range(-randomDistance, randomDistance),
+				0, Random.Range(-randomDistance, randomDistance));
+			transform.parent.position += teleportVector;
+			transform.parent.GetComponent<Player>().net += teleportVector;
+            Debug.Log("Spell used");
+
+
+        }
+	}
+}
