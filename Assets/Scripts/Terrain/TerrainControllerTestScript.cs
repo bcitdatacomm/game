c4727fe3265eeae4f62c3b9fa586a7b48a9c4225
rogerzhang18 <rogeryj.zhang@gmail.com>
2018-03-21 15:38:04 -0700

    added weapon spawn
    Signed-off-by: rogerzhang18 <rogeryj.zhang@gmail.com>

--- Assets/Scripts/Terrain/TerrainControllerTestScript.cs
+++ Assets/Scripts/Terrain/TerrainControllerTestScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InitGuns;
 
 // Assign this to an empty game object in order to test the Terrain Controller
 public class TerrainControllerTestScript : MonoBehaviour
@@ -9,7 +10,11 @@ public class TerrainControllerTestScript : MonoBehaviour
     // Use this for initialization of the Terrain Object, and for testing function calls
     void Start()
     {
+        InitRandomGuns ic = new InitRandomGuns(30, null);
+
         TerrainController tc = new TerrainController();
+        InitRandomGuns icClient = new InitRandomGuns();
+        tc.LoadGuns(ic.pcktarray);
         tc.GenerateEncoding();
         tc.Instantiate();
     }
