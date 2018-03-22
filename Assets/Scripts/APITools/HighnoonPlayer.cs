acae2c10343e3e1100729211abc9deac580767af
MorganAriss <morganariss93@gmail.com>
2018-03-21 19:00:01 -0700

    Added login and registration from unity GUI

--- Assets/Scripts/APITools/HighnoonPlayer.cs
+++ Assets/Scripts/APITools/HighnoonPlayer.cs
@@ -13,14 +13,14 @@ namespace HighnoonTools
         int _games_won;
         int _time_played; // TODO: is this seconds?
 
-        public string Name { get { return _name; } }
-        public bool Admin { get { return _admin; } }
-        public int Shots_Fired { get { return _shots_fired; } }
-        public int Kills { get { return _kills; } }
-        public int Deaths { get { return _deaths; } }
-        public int Games_Played { get { return _games_played; } }
-        public int Games_Won { get { return _games_won; } }
-        public int Time_Played { get { return _time_played; } }
+		public string Name { get { return _name; } set { _name = value; } }
+		public bool Admin { get { return _admin; } set { _admin = value; } }
+		public int Shots_Fired { get { return _shots_fired; } set { _shots_fired = value; } }
+		public int Kills { get { return _kills; } set { _kills = value; } }
+		public int Deaths { get { return _deaths; } set { _deaths = value; } }
+		public int Games_Played { get { return _games_played; } set { _games_played = value; } }
+		public int Games_Won { get { return _games_won; } set { _games_won = value; } }
+		public int Time_Played { get { return _time_played; } set { _time_played = value; } }
     }
 }
 
