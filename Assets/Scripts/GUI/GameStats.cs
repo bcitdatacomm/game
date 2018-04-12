3e984b4de460a571b5acb24aa2a7e74d8dca6e7e
Luke <luke.lee7575@gmail.com>
2018-04-11 19:21:37 -0700

    Fixed minimap and floaty guns

---
+++ Assets/Scripts/GUI/GameStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour
+{
+	public Text timerLabel;
+
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		time += Time.deltaTime;
+
+		var minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+		var seconds = time % 60;//Use the euclidean division for the seconds.
+		var fraction = (time * 100) % 100;
+
+		//update the label value
+		timerLabel.text = string.Format ("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+	}
+}
