954b861ebe2b028285cb5a43340bc188ff640c0a
MorganAriss <morganariss93@gmail.com>
2018-04-12 06:12:47 -0700

    You win or you die...

---
+++ Assets/Scripts/Menu/ReturnToMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMain : MonoBehaviour {
+
+	public Button returnButton;
+	public int sceneIndex;
+
+	// Use this for initialization
+	void Start () {
+
+
+		returnButton.onClick.AddListener( () => {returnFunction();} );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void returnFunction()
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+}
